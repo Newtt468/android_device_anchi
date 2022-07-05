ab688524f6eee3e8646680becd1e086d8826d7b6
Newtt468 <108245520+Newtt468@users.noreply.github.com>
2022-07-05 16:24:37 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@
 -include vendor/Lenovo/anchi/BoardConfigVendor.mk
 
 # GPS
-TARGET_SPECIFIC_HEADER_PATH := device/Xiaomi/hm_note_1w/include
+TARGET_SPECIFIC_HEADER_PATH := device/Lenovo/anchi/include
 
 # Platform
 TARGET_BOARD_PLATFORM := mt6592
@@ -36,9 +36,9 @@ BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 #TARGET_KERNEL_CONFIG := 2014011_debug_defconfig
-#TARGET_KERNEL_SOURCE := Xiaomi/hm_note_1w\
-TARGET_PREBUILT_KERNEL := device/Xiaomi/hm_note_1w/kernel
-BOARD_CUSTOM_BOOTIMG_MK := device/Xiaomi/hm_note_1w/bootimg.mk
+#TARGET_KERNEL_SOURCE := Lenovo/anchi\
+TARGET_PREBUILT_KERNEL := device/Lenovo/anchi/kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/Lenovo/anchi/bootimg.mk
 BOARD_CUSTOM_BOOTIMG := true
 TARGET_KMODULES := true
 
@@ -52,7 +52,7 @@ TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 TARGET_CPU_MEMCPY_OPT_DISABLE := true
 
 # EGL
-BOARD_EGL_CFG := device/Xiaomi/hm_note_1w/configs/egl.cfg
+BOARD_EGL_CFG := device/Lenovo/anchi/configs/egl.cfg
 USE_OPENGL_RENDERER := true
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
 BOARD_EGL_NEEDS_HANDLE_VALUE := true
@@ -70,7 +70,7 @@ BLOCK_BASED_OTA :=false
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/BOOT/BOOT/boot/boot_mode
 
 # RIL
-BOARD_RIL_CLASS := ../../../device/Xiaomi/hm_note_1w/ril/
+BOARD_RIL_CLASS := ../../../device/Lenovo/anchi/ril/
 
 BOARD_CONNECTIVITY_VENDOR := MediaTek
 BOARD_CONNECTIVITY_MODULE := conn_soc
@@ -90,19 +90,19 @@ WIFI_DRIVER_FW_PATH_P2P:=P2P
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_MTK := true
 BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/Xiaomi/hm_note_1w/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/Lenovo/anchi/bluetooth
 
 # Sensors
 TARGET_NO_SENSOR_PERMISSION_CHECK := true
 
 # CWM
-TARGET_RECOVERY_FSTAB := device/Xiaomi/hm_note_1w/rootdir/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/Lenovo/anchi/rootdir/recovery.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP
 #RECOVERY_VARIANT=twrp
 TW_THEME := portrait_hdpi
-DEVICE_RESOLUTION := 720x1280
+DEVICE_RESOLUTION := 540x960
 BOARD_HAS_LARGE_FILESYSTEM := true
 TW_NO_USB_STORAGE := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
@@ -119,7 +119,7 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
 
 BOARD_SEPOLICY_DIRS := \
-       device/Xiaomi/hm_note_1w/sepolicy
+       device/Lenovo/anchi/sepolicy
        
 
 # Enable Minikin text layout engine (will be the default soon)
